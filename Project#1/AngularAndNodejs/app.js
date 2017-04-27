3dfd8133984200b32340c67f546aec18e6bf4d63
choi <zealousca@gmail.com>
2017-04-27 10:49:10 +0900

    modify readme

--- Project#1/AngularAndNodejs/app.js
+++ Project#1/AngularAndNodejs/app.js
@@ -8,14 +8,14 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+//서버와 통신을 주고 받을 Js파일을 지정해줍시다.
+var rest_api = require('./routes/rest_api');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,27 +25,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
 
-db.serialize(function() {
 
-  db.run('CREATE TABLE lorem (info TEXT)');
-  var stmt = db.prepare('INSERT INTO lorem VALUES (?)');
+//라우팅은 URI(또는 경로) 및 특정한 HTTP 요청 메소드(GET, POST 등)인 특정 엔드포인트에 대한 클라이언트 요청에 애플리케이션이 응답하는 방법을 결정하는 것을 말합니다.
+//app.METHOD(PATH, HANDLER)
+//app은 express의 인스턴스입니다.
+//METHOD는 HTTP 요청 메소드입니다.
+//PATH는 서버에서의 경로입니다.
+//HANDLER는 라우트가 일치할 때 실행되는 함수입니다.
 
-  for (var i = 0; i < 10; i++) {
-    stmt.run('Ipsum ' + i);
+//req = require Frontend에서 필요한 요청을 Backend에 전달하기 위한 변수
+//res = responce Backend에서 처리한 Frontend의 요청에 답하기 위한 변수
+app.all('/api/add', function (req, res, next) {
+  console.log('Add API is called...');
+  //
+  api = new rest_api(); 
+  result = api.write(req);
+  if (result) {
+    res.json({success: true})
   }
-
-  stmt.finalize();
-
-  db.each('SELECT rowid AS id, info FROM lorem', function(err, row) {
-    console.log(row.id + ': ' + row.info);
-  });
 });
 
-db.close();
+app.all('/api/list', function (req, res, next) {
+  console.log('List API is called...');
+  api = new rest_api();
+  api.list(req, res);
+});
 
+//페이지 에러에 대한 옵션들
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
---
+++ Project#1/AngularAndNodejs/routes/rest_api.js
@@ -0,0 +1,49 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(':memory:');
+
+class rest_api {
+  constructor() {
+    db.serialize(function () {
+      db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='note'", function(error, row) {
+      		if (row !== undefined) {
+      			console.log("Table exists. skipping creation")
+      		}	else {
+      			console.log("Creating table")
+      			db.run("CREATE TABLE note (id INTEGER PRIMARY KEY AUTOINCREMENT, created TIMESTAMP DEFAULT (DATETIME('now')), content TEXT)", function(error) {
+              if (error != null) {
+        				if (error.message.indexOf("already exists") != -1) {
+        					console.log(error);
+        				}
+              }
+      			})
+      		}
+      	})
+    })
+  };
+
+  write(req) {
+    db.serialize(function () {
+      var stmt = db.prepare('INSERT INTO note (content) VALUES (?)')
+      stmt.run(req.body.content)
+      stmt.finalize()
+      //db.run('INSERT INTO note (content) VALUES ('+req.body.content+')')
+    })
+    return true
+  };
+
+  list(req, res) {
+    return db.serialize(function () {
+      var items = [];
+      db.each('SELECT id, created, content FROM note', function (err, row) {
+        items.push({id: row.id, created: row.created, content: row.content})
+      }, function() {
+        console.log(items)
+        if (items) {
+          res.json({success: true, item: items})
+        }
+      })
+    })
+  }
+}
+
+module.exports = rest_api;
